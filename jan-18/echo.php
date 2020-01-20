6f201f97a59d2922d717293c1fd77117bae1e815
Aayusha <imaayushapatel@gmail.com>
2020-01-20 18:34:43 +0530

    echo and print statement

---
+++ jan-18/echo.php
@@ -0,0 +1,26 @@
+ <?php
+ $text1="Hello";
+ $text2="World";
+ echo $text1." ".$text2;
+ echo "<br><b>$text1 $text2 !!</b>";
+ echo("<br>Hello World");
+ echo("<br> ".$text1." ".$text2);
+ $x=2 | 3; //bitwies OR
+ $y=2;
+ echo $x +$y;
+ //echo $text1 + $text2;
+ //echo "The sum is " . 20 | 10; // output: "2". Parentheses needed.
+echo "The sum is ", 1 | 2; // output: "The sum is 3". Fine.
+//echo "Sum is : ". $x + $y;
+echo "Sum is : ", $x + $y;
+ ?>
+ <br>
+ I have <?=$text1?> foo
+
+ <?php
+    echo "<input type='text'><br>";
+    echo "<input type=\"text\"><br>";
+    echo '<input type="text"><br>';
+?>
+
+ 
---
+++ jan-18/print.php
@@ -0,0 +1,19 @@
+ <?php
+ $text1="Hello";
+ $text2="World";
+ print $text1." ".$text2;
+ print "<br><b>$text1 $text2 !!</b>";
+ print("<br>Hello World");
+ print("<br> ".$text1." ".$text2);
+ $x=2 | 3; //bitwies OR
+ $y=2;
+ print $x +$y;
+ // , in print does not work
+print "The sum is ". 1 | 2; // output: "The sum is 3". Fine.
+//print "Sum is : ". $x + $y;
+print "Sum is : ". $x + $y;
+ ?>
+ <br>
+
+
+ 
