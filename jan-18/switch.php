570295ad2759628e0d4890a93b9151abaad562a3
Aayusha <imaayushapatel@gmail.com>
2020-01-22 00:52:43 +0530

    switch statement

---
+++ jan-18/switch.php
@@ -0,0 +1,15 @@
+<?php
+$i = 2;
+switch ($i%2) {
+    case 0:
+        echo "even";
+        break;
+    case 1:
+        echo "odd";
+        break;
+    
+    default:
+        # code...
+        break;
+}
+?>
---
+++ jan-18/switch_1.php
@@ -0,0 +1,16 @@
+<?php
+$a = 'a';
+    switch ($a) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            echo "It is a vowel";
+        break;
+        
+        default:
+            echo "It is not a vowel";
+            break;
+    }
+?>
