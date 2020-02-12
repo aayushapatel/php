d8f3cb8d351807b8f3cbe5cf5f5183f3991a9f36
Aayusha <imaayushapatel@gmail.com>
2020-02-12 10:03:52 +0530

    mvc till controller and view

--- mvc/public/index.php
+++ mvc/public/index.php
@@ -1,14 +1,14 @@
 <?php
-    require_once dirname(__DIR__) . '\vendor\autoload.php';
     
-    spl_autoload_register(function ($className) {
-        $root = str_replace('\\', '/', dirname(__DIR__));
-        $file = $root . '/' . str_replace('\\', '/', $className) . '.php';
+    require '../vendor/autoload.php';
+    // spl_autoload_register(function ($className) {
+    //     $root = str_replace('\\', '/', dirname(__DIR__));
+    //     $file = $root . '/' . str_replace('\\', '/', $className) . '.php';
         
-        if(is_readable($file)) {
-           require $file;
-        }
-    });
+    //     if(is_readable($file)) {
+    //        require $file;
+    //     }
+    // });
     $router = new Core\Router();
 
 ?>    
--- mvc/vendor/composer/autoload_static.php
+++ mvc/vendor/composer/autoload_static.php
@@ -21,6 +21,14 @@ class ComposerStaticInit9cc7c940e3034ec15164f5ae41ec8d22
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
         ),
+        'C' => 
+        array (
+            'Core\\' => 5,
+        ),
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -35,6 +43,14 @@ class ComposerStaticInit9cc7c940e3034ec15164f5ae41ec8d22
         'Symfony\\Polyfill\\Ctype\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
+        'Core\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/Core',
+        ),
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/App',
         ),
     );
 
